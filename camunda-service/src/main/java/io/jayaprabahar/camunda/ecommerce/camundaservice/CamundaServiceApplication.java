5827ff3f70f278f9da436cbbc7b63c9695e90bfc
Jayaprabahar <jpofficial@gmail.com>
2025-09-14 11:55:25 +0200

    chore(docs): refine README and add JavaDoc to main application classes

--- camunda-service/src/main/java/io/jayaprabahar/camunda/ecommerce/camundaservice/CamundaServiceApplication.java
+++ camunda-service/src/main/java/io/jayaprabahar/camunda/ecommerce/camundaservice/CamundaServiceApplication.java
@@ -5,11 +5,24 @@ import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Camunda orchestration service application.
+ *
+ * <p>This Spring Boot application boots the Zeebe/Camunda client and deploys
+ * BPMN workflow definitions found on the classpath. It serves as the central
+ * orchestrator for the example e-commerce checkout workflows in this
+ * repository.
+ */
 @SpringBootApplication
 @EnableZeebeClient
 @Deployment(resources = "classpath*:/bpmn/**/*.bpmn")
 public class CamundaServiceApplication {
 
+	/**
+	 * Application entry point.
+	 *
+	 * @param args standard Spring Boot args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CamundaServiceApplication.class, args);
 	}
